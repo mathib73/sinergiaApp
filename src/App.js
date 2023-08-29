8bb25746fabae5914d3265f3bf62b9e2985d0860
¨Mathias <mathib73@gmail.com>
2023-08-29 20:53:36 -0300

    mailto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import landing from './landing.png';
 import './App.css';
 
+const mailTo = (e) => {
+  window.location.href = "mailto:contacto@dogtechdog.com";
+  e.preventDefault();
+}
 function App() {
   return (
     <div className="App">
       <div className="row">
+        <div className="contact" onClick={mailTo}/>
         <div className="container responsive" />
       </div>
     </div>
